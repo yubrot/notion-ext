a35993ca8f6f3a6c67fde7276d1651a8e4cae7fa
yubrot <yubrot@gmail.com>
2025-05-08 11:26:06 +0900

    Fix prettierignore and eslint config to avoid generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,5 @@ import tseslint from 'typescript-eslint'
 import eslint from '@eslint/js'
 
 export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic, {
-  ignores: ['**/dist/**'],
+  ignores: ['**/{dist,docs,coverage,node_modules}/**', 'migrator-example/src/prisma/client/**'],
 })
